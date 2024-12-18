5703ff660ce4e48b7f7079402d4933faf74aa1df
FeZZus <farazfez7865@gmail.com>
2024-12-18 21:30:56 +0000

    hello there

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import App from './App';
 
 // THis is where react injects the code into the HTML code
-// The HTML remains otherwise unchanged
+// The HTML remains otherwise unchanged 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
