54b9600b6846d58432bd1d744e45e86df5fbd4ce
FeZZus <farazfez7865@gmail.com>
2024-12-18 21:24:16 +0000

    Commit title
    commit descirption

---
+++ src/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+import App from './App';
+
+// THis is where react injects the code into the HTML code
+// The HTML remains otherwise unchanged
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);
